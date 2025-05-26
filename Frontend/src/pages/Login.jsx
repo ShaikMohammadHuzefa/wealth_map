74fa31525b29483c6bb6ccb6e018cbc3b60ca4aa
ShaikMohammadHuzefa <154452155+ShaikMohammadHuzefa@users.noreply.github.com>
2025-05-26 16:34:43 +0530

    Update Login.jsx

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
 
     const handleLogin = async (e) => {
         e.preventDefault();
-        const response = await fetch("http://localhost:3000/login", {
+        const response = await fetch("https://wealth-map-backend.onrender.com/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(credentials),
